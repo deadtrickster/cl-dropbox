78a28d7c7127216339f402971c7dd143868eec81
Ilya Khaprov <dead.trickster@gmail.com>
2014-09-25 21:01:29 -0700

    trusted-certificates-file-not-found error

--- src/conditions.lisp
+++ src/conditions.lisp
@@ -2,9 +2,15 @@
 ;;;
 ;;; See LICENSE for details.
 
-(in-package :cl-dropbox)
+(in-package :cl-user)
 
-(define-condition cl-dropbox-error-base ()
+(defpackage :cl-dropbox.conditions
+  (:use :cl)
+  (:export #:trusted-certificates-file-not-found))
+
+(in-package :cl-dropbox.conditions)
+
+(define-condition cl-dropbox-error-base (error)
   ()
   (:documentation "Base error class for all cl-dropbox errors"))
 
@@ -15,3 +21,9 @@
 (define-condition cl-dropbox-error (cl-dropbox-error-base)
   ()
   (:documentation "Base error class for errors occured in cl-dropbox library itself"))
+
+(define-condition trusted-certificates-file-not-found (cl-dropbox-error)
+  ((pathname :initarg :pathname))
+  (:documentation "Unable to find trusted certificates. Used as cafile")
+  (:report (lambda (condition stream)
+             (format stream "Unable to find trusted certificates. Path: ~A" (slot-value condition 'pathname)))))
