b090a9f87ed2d9de24eb96f8b8874b44d392fe62
Ilya Khaprov <dead.trickster@gmail.com>
2014-09-27 16:09:33 -0700

    mover error handling to dropbox.lisp, fix url generation, etc

--- src/http.lisp
+++ src/http.lisp
@@ -7,11 +7,17 @@
 (cl-interpol:enable-interpol-syntax)
 
 (defclass request ()
-  ((headers :initarg :headers :accessor request-headers)
-   (body :initarg :body :accessor request-body)
-   (method :initarg :method :accessor request-method)
-   (path :initarg :uri :accessor request-path)
-   (params :initarg :query :accessor request-params)))
+  ((headers :initform nil :initarg :headers :accessor request-headers)
+   (body :initform nil  :initarg :body :accessor request-body)
+   (method :initform :get  :initarg :method :accessor request-method)
+   (path :initform nil  :initarg :path :accessor request-path)
+   (params :initform nil  :initarg :params :accessor request-params)))
+
+(defclass response ()
+  ((status-code :initarg :status-code :reader response-status-code)
+   (headers :initarg :headers :reader response-headers)
+   (body :initarg :body :reader response-body)))
+
 
 (defgeneric request-url (request))
 
@@ -19,28 +25,28 @@
   ())
 
 (defmethod request-url ((request api-request))
-  (make-instance 'puri:uri :scheme "https"
+  (make-instance 'puri:uri :scheme :https
                            :host +api-server+
-                           :path #?"#{+dropbox-api-version+}/#{request-path}"))
+                           :path #?"/${+api-version+}/${(request-path request)}"))
 
 (defclass content-request (request)
   ())
 
 (defmethod request-url ((request content-request))
-  (make-instance 'puri:uri :scheme "https"
+  (make-instance 'puri:uri :scheme :https
                            :host +api-content-server+
-                           :path #?"#{+api-version+}/#{request-path}"))
+                           :path #?"/${+api-version+}/${(request-path request)}"))
 
-(defun add-header (request name value)
-  (setf (request-headers request) (acons name value (request-headers request))))
 
-(defclass response ()
-  ((status-code :initarg :status-code :reader response-status-code)
-   (headers :initarg :headers :reader response-headers)
-   (body :initarg :body :reader response-body)))
-
-(defun response-header (response name)
+(defun header-value (response name)
   (drakma:header-value name (response-headers response)))
+
+(defun (setf header-value) (value request name)
+  ;;check if header with name already presented, if true change its value otherwise add new header
+  (let ((header (assoc name (request-headers request) :test #'string-equal)))
+    (if header
+        (rplacd header value)
+        (setf (request-headers request) (acons name value (request-headers request))))))
 
 (defun http-request-with-ssl (request)
   ;; do not replace cl+ssl global state, rebind
@@ -58,50 +64,9 @@
       response
       (trivial-utf-8:utf-8-bytes-to-string response)))
 
-(defun ensure-string-and-success (response status-code headers)
-  (let ((response-string (ensure-string response))
-        (ret))
-    (cond
-      ((and (>= 200 status-code)
-            (< 300 status-code))
-       (setf ret response))
-      ((= status-code 400)
-       (setq ret 'cl-dropbox-api-bad-input))
-      ((= status-code 401)
-       (setq ret 'cl-dropbox-api-unauthorized))
-      ((= status-code 403)
-       (setq ret 'cl-dropbox-api-forbidden))
-      ((= status-code 404)
-       (setq ret 'cl-dropbox-api-notfound))
-      ((= status-code 405)
-       (setq ret 'cl-dropbox-api-method-not-expected))
-      ((= status-code 429)
-       (setq ret 'cl-dropbox-api-too-many-requests))
-      ((= status-code 503)
-       (if (drakma:header-value :retry-after headers)
-           (setq ret 'cl-dropbox-api-oauth1.0-rate-limited)
-           (setq ret 'cl-dropbox-api-transient-server-error)))
-      ((= status-code 507)
-       (setq ret 'cl-dropbox-api-over-quota))
-      ((and (>= status-code 500) (< status-code 600))
-       (setq ret 'cl-dropbox-api-server-error))
-      (t 'cl-dropbox-api-error-non-standard))
-    (if (symbolp ret)
-        (error ret :status-code status-code :response response-string :headers headers)
-        response-string)))
-
 (defun http-request% (request)
   (multiple-value-bind (response status-code headers)
       (http-request-with-ssl request)
-    (values (ensure-string-and-success response status-code headers) headers)))
+    (make-instance 'response :body response :headers headers :status-code status-code)))
 
-(defun http-request (request)
-  (let ((retries 0))
-    (tagbody
-     :retry
-       (handler-bind ((cl-dropbox-transient-server-error (lambda (c)
-                                                           (log:error c)
-                                                           (when (< retries *max-dropbox-server-error-retries*) (incf retries) (go :retry))))
-                      (error (lambda (c)
-                               (log:error c))))
-         (http-request% request)))))
+
