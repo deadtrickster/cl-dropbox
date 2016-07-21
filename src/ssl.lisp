0c8c1b794b5ce1745aec9f8cb30de2f4509a2586
Ilya Khaprov <dead.trickster@gmail.com>
2016-07-21 12:11:55 +0300

    use new cl+ssl

--- src/ssl.lisp
+++ src/ssl.lisp
@@ -10,54 +10,6 @@
 
 (in-package :cl-dropbox.ssl)
 
-(define-condition ssl-error-call (cl+ssl::ssl-error)
-  ((message :initarg :message))
-  (:documentation
-   "A failure in the SSL library occurred..")
-  (:report (lambda (condition stream)
-             (format stream "A failure in OpenSSL library occurred~@[: ~A~].~%" (slot-value condition 'message)) (cl+ssl::format-ssl-error-queue stream (cl+ssl::ssl-error-queue condition)))))
-
-(defun ssl-signal-error (&optional message)
-  (error 'ssl-error-call :queue (cl+ssl::read-ssl-error-queue) :message message))
-
-(defun get-cafile-namestring (file)
-  (handler-case
-      (if (pathnamep file)
-          (namestring (truename file))
-          (namestring (or (probe-file file) (error 'file-error :pathname file))))
-    (file-error (c)
-      (declare (ignorable c))
-      (error 'trusted-certificates-file-not-found :pathname file))))
-
-(defun add-verify-files% (ssl-ctx files)
-  (dolist (file files)
-    (let ((namestring (get-cafile-namestring file)))
-      (cffi:with-foreign-strings ((cafile namestring))
-        (when (eql 0 (cl+ssl::ssl-ctx-load-verify-locations
-                      ssl-ctx
-                      cafile
-                      (cffi:null-pointer)))
-          (ssl-signal-error (format nil "Unable to load cafile ~A" namestring)))))))
-
-(defun add-verify-files (ssl-ctx files)
-  (cond
-    ((stringp files)
-     (add-verify-files% ssl-ctx (list files)))
-    ((pathnamep files)
-     (add-verify-files% ssl-ctx (list files)))
-    (t nil)))
-
-(cffi:defcfun ("SSL_CTX_set_cipher_list" ssl-ctx-set-cipher-list%)
-    :int
-  (ctx :pointer)
-  (ciphers :pointer))
-
-(defun ssl-ctx-set-cipher-list (ctx ciphers)
-  (cffi:with-foreign-string (ciphers* ciphers)
-    (when (eql 0 (ssl-ctx-set-cipher-list% ctx ciphers*))
-      (ssl-signal-error))))
-
-
 (let ((ssl-ctx))
   (defun ssl-context ()
     "SSL context for Dropbox REST API connections"
@@ -67,31 +19,5 @@
       ;; we want to create context before any call to make-ssl-client-stream
       (handler-bind ((error #'(lambda (c) (setf ssl-ctx nil) (log:error "Error while creating SSL Context: ~A" c))))
         (cl+ssl::ensure-initialized)
-        (setf ssl-ctx (cl+ssl::ssl-ctx-new (cl+ssl::ssl-tlsv1-client-method)))
-        (cl+ssl::ssl-ctx-set-session-cache-mode ssl-ctx cl+ssl::+ssl-sess-cache-client+)
-        (add-verify-files ssl-ctx (merge-pathnames #p"data/trusted-certs.crt" (asdf:component-pathname (asdf:find-system :cl-dropbox))))
-        (cl+ssl::ssl-ctx-set-verify-depth ssl-ctx 100)
-        (cl+ssl::ssl-ctx-set-verify ssl-ctx cl+ssl::+SSL-VERIFY-PEER+ (cffi:callback cl+ssl::cb-ssl-verify))
-        (ssl-ctx-set-cipher-list ssl-ctx (format nil
-                                                 "ECDHE-RSA-AES256-GCM-SHA384:~
-                                                  ECDHE-RSA-AES256-SHA384:~
-                                                  ECDHE-RSA-AES256-SHA:~
-                                                  ECDHE-RSA-AES128-GCM-SHA256:~
-                                                  ECDHE-RSA-AES128-SHA256:~
-                                                  ECDHE-RSA-AES128-SHA:~
-                                                  ECDHE-RSA-RC4-SHA:~
-                                                  DHE-RSA-AES256-GCM-SHA384:~
-                                                  DHE-RSA-AES256-SHA256:~
-                                                  DHE-RSA-AES256-SHA:~
-                                                  DHE-RSA-AES128-GCM-SHA256:~
-                                                  DHE-RSA-AES128-SHA256:~
-                                                  DHE-RSA-AES128-SHA:~
-                                                  AES256-GCM-SHA384:~
-                                                  AES256-SHA256:~
-                                                  AES256-SHA:~
-                                                  AES128-GCM-SHA256:~
-                                                  AES128-SHA256:~
-                                                  AES128-SHA"))
-        ;; TODO more session handling
-        ))
+        (setf ssl-ctx (cl+ssl:make-context :verify-location (merge-pathnames #p"data/trusted-certs.crt" (asdf:component-pathname (asdf:find-system :cl-dropbox)))))))
     ssl-ctx))
